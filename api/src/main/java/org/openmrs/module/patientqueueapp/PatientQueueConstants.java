be41a11170d337c61d2a032dd931f5a5bd5a95e3
ningosi <nicotwendelee@gmail.com>
2020-09-29 13:12:26 +0300

    Add the right app ids for the OPD and Triage apps

--- api/src/main/java/org/openmrs/module/patientqueueapp/PatientQueueConstants.java
+++ api/src/main/java/org/openmrs/module/patientqueueapp/PatientQueueConstants.java
@@ -2,11 +2,12 @@ package org.openmrs.module.patientqueueapp;
 
 public class PatientQueueConstants {
     public static final String MODULE_ID ="patientqueueapp" ;
+    public static final String MODULE_ID_1 ="patientdashboardapp" ;
     public static final String APP_DELIVERY_ROOM = MODULE_ID+".delivery";
     public static final String APP_MATERNITY_TRIAGE = MODULE_ID+".maternitytriage";
     public static final String APP_MCH_CLINIC = MODULE_ID+".mchclinic";
     public static final String APP_MCH_TRIAGE = MODULE_ID+".mchtriage";
-    public static final String APP_OPD = MODULE_ID+".opdqueue";
+    public static final String APP_OPD = MODULE_ID_1+".opdqueue";
     public static final String APP_PAC = MODULE_ID+".pac";
-    public static final String APP_TRIAGE = MODULE_ID+".triage";
+    public static final String APP_TRIAGE = MODULE_ID_1+".triage";
 }
