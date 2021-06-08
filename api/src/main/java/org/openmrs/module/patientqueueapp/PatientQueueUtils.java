1464d459c749b7d59277bf30584a3b261836e875
VIC <ngarivictor43@gmail.com>
2021-06-08 15:57:43 +0300

    get role by name

--- api/src/main/java/org/openmrs/module/patientqueueapp/PatientQueueUtils.java
+++ api/src/main/java/org/openmrs/module/patientqueueapp/PatientQueueUtils.java
@@ -19,11 +19,11 @@ public class PatientQueueUtils {
     public static final String MCH_TRIAGE_USER_FP_QUEUE = "8fa718ef-550a-4d71-8ff9-a61174e9e65f";
     public static final String MCH_TRIAGE_USER_CWC_QUEUE = "1c54b4e5-ac8a-4ac3-bff2-a87192c9557f";
 
-    public static final String MCH_CLINIC_USER_ANC_QUEUE = "d13cb3e6-a5ec-4c76-b405-a469aa7c6865";
-    public static final String MCH_CLINIC_USER_PNC_QUEUE = "ee748fb3-7244-4b80-9ac3-24175a358a2d";
-    public static final String MCH_CLINIC_USER_FP_QUEUE = "da78bc6d-01b8-48b5-bb07-0b6a302fe7f7";
-    public static final String MCH_CLINIC_USER_CWC_QUEUE = "43635304-bdfc-4ee9-8053-d79c0d1f1084";
-    public static final String MCH_IMMUNIZATION_CWC_QUEUE = "9fbabc78-abc5-4cab-bd02-c16377568bd0";
+    public static final String MCH_CLINIC_USER_ANC_QUEUE = "MCH Clinic user for ANC";
+    public static final String MCH_CLINIC_USER_PNC_QUEUE = "MCH Clinic user for PNC";
+    public static final String MCH_CLINIC_USER_FP_QUEUE = "MCH Clinic user for FP";
+    public static final String MCH_CLINIC_USER_CWC_QUEUE = "MCH Clinic user for CWC";
+    public static final String MCH_IMMUNIZATION_CWC_QUEUE = "CWC Immunization Queue";
 
     public static final String EXAM_ROOM_CONCEPT_UUID = "159937AAAAAAAAAAAAAAAAAAAAAAAAAAAAA";//  MCH program
     public static final String IMMUNIZATION_ROOM_CONCEPT_UUID = "5f819604-ca2c-4567-95f3-91b8fafb3305"; //MCH Immunization
@@ -40,7 +40,7 @@ public class PatientQueueUtils {
                 }
             }
             else if (clinic.equals("Clinic")){
-                if (role.getUuid().equals(MCH_CLINIC_USER_ANC_QUEUE) || role.getUuid().equals(MCH_CLINIC_USER_PNC_QUEUE) || role.getUuid().equals(MCH_CLINIC_USER_CWC_QUEUE) || role.getUuid().equals(MCH_CLINIC_USER_FP_QUEUE) || role.getUuid().equals(MCH_IMMUNIZATION_CWC_QUEUE)) {
+                if (role.getRole().equals(MCH_CLINIC_USER_ANC_QUEUE) || role.getRole().equals(MCH_CLINIC_USER_PNC_QUEUE) || role.getRole().equals(MCH_CLINIC_USER_CWC_QUEUE) || role.getRole().equals(MCH_CLINIC_USER_FP_QUEUE) || role.getRole().equals(MCH_IMMUNIZATION_CWC_QUEUE)) {
                     mchRoles.add(role);
                 }
             }
