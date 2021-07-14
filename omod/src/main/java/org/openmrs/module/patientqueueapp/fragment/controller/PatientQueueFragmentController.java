8e262aef51c9f5a98b98459748224886d1347be6
ningosi <nicotwendelee@gmail.com>
2021-07-14 15:58:36 +0300

    Add the routine to separate answers from questions that might be already using the answers founf in the EHR

--- omod/src/main/java/org/openmrs/module/patientqueueapp/fragment/controller/PatientQueueFragmentController.java
+++ omod/src/main/java/org/openmrs/module/patientqueueapp/fragment/controller/PatientQueueFragmentController.java
@@ -1,6 +1,4 @@
 package org.openmrs.module.patientqueueapp.fragment.controller;
-
-
 import org.openmrs.Concept;
 import org.openmrs.ConceptAnswer;
 import org.openmrs.Encounter;
@@ -17,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
@@ -96,7 +95,7 @@ public class PatientQueueFragmentController {
 			patientInQueue.put("id", patientQueue.getId());
 
 			Concept oPdConcept = patientQueue.getOpdConcept();
-			 if (oPdConcept.equals(fpRoomConcept)){
+			if (oPdConcept.equals(fpRoomConcept)){
 				patientInQueue.put("clinic","FP");
 			}
 			else if(oPdConcept.equals(mchExamRoomConcept))
@@ -114,33 +113,36 @@ public class PatientQueueFragmentController {
 
 	public SimpleObject getPatientsInQueue(@RequestParam("opdId") Integer opdId, @RequestParam(value = "query", required = false) String query, UiUtils ui) {
 		Concept queueConcept = Context.getConceptService().getConcept(opdId);
-		ConceptAnswer queueAnswer = Context.getService(PatientQueueService.class).getConceptAnswer(queueConcept);
+
+		//get teh quetion here from the given answer
+		Concept question = getTheQuestionToTheGivenAnswer(queueConcept);
 
 
 		SimpleObject patientQueueData = null;
 
-		if (queueAnswer != null && queueAnswer.getConcept() != null && queueAnswer.getConcept().getName().toString().equals("Triage Room")) {
+
+		if (question != null && question.equals(Context.getConceptService().getConceptByUuid("e8acf3d5-d451-475b-a3b5-37f0ce6a0260"))) {
 			List<TriagePatientQueue> patientQueues = Context.getService(PatientQueueService.class).listTriagePatientQueue(query.trim(), opdId, "", 0, 0);
 			List<SimpleObject> patientQueueObject = SimpleObject.fromCollection(patientQueues, ui, "patientName", "patientIdentifier", "age", "sex", "status", "visitStatus","patient.id", "id");
 			patientQueueData = SimpleObject.create("data", patientQueueObject, "user", "triageUser");
-		} else if (queueAnswer != null && queueAnswer.getConcept() != null && queueAnswer.getConcept().getName().toString().equals("OPD Room")) {
+		} else if (question != null && question.equals(Context.getConceptService().getConceptByUuid("03880388-07ce-4961-abe7-0e58f787dd23"))) {
 			List<OpdPatientQueue> patientQueues = Context.getService(PatientQueueService.class).listOpdPatientQueue(query.trim(), opdId, "", 0, 0);
 			List<SimpleObject> patientQueueObject = SimpleObject.fromCollection(patientQueues, ui, "patientName", "patientIdentifier", "age", "sex", "status", "visitStatus","patient.id", "id", "referralConcept.conceptId");
 			patientQueueData = SimpleObject.create("data", patientQueueObject, "user", "opdUser");
-		} else if(queueAnswer != null && queueAnswer.getConcept() != null && queueAnswer.getConcept().getName().toString().equals("Special Clinic")) {
+		} else if(question != null && question.equals(Context.getConceptService().getConceptByUuid("b5e0cfd3-1009-4527-8e36-83b5e902b3ea"))) {
 			List<OpdPatientQueue> patientQueues = Context.getService(PatientQueueService.class).listOpdPatientQueue(query.trim(), opdId, "", 0, 0);
 			List<SimpleObject> patientQueueObject = SimpleObject.fromCollection(patientQueues, ui, "patientName", "patientIdentifier", "age", "sex", "status", "visitStatus","patient.id", "id", "referralConcept.conceptId");
 			patientQueueData = SimpleObject.create("data", patientQueueObject, "user", "opdUser");
 		}
 		return patientQueueData;
 	}
-	
+
 	public SimpleObject addPatientToQueue(
 			@RequestParam("patientId") Integer patientId,
 			@RequestParam("opdId") Integer opdId) {
 		Patient patient = Context.getPatientService().getPatient(patientId);
 		PatientQueueService queueService = Context.getService(PatientQueueService.class);
-		
+
 		List<OpdPatientQueue> matchingPatientsInQueue = queueService.listOpdPatientQueue(patient.getPatientIdentifier().getIdentifier(), opdId, "", 0, 0);
 		OpdPatientQueue patientInQueue = null;
 		List<Encounter> existingEncounters = Context.getEncounterService().getEncounters(patient, null, null, null, null, null, null, null, null, false);
@@ -178,7 +180,7 @@ public class PatientQueueFragmentController {
 				patientInQueue.setPatientName(patient.getGivenName() + " " + patient.getFamilyName());
 			}
 			updatePatientQueueDataFromPreviousVisit(patientInQueue, patient, queueService);
-			
+
 			patientInQueue = queueService.saveOpdPatientQueue(patientInQueue);
 		} else {
 			patientInQueue = matchingPatientsInQueue.get(0);
@@ -200,5 +202,35 @@ public class PatientQueueFragmentController {
 				patientInQueue.setVisitStatus(visitStatus);
 			}
 		}
+	}
+
+	private Concept getTheQuestionToTheGivenAnswer(Concept concept) {
+		Concept foundQuestion = null;
+		Collection<ConceptAnswer> triageAnswers = Context.getConceptService().getConceptByUuid("e8acf3d5-d451-475b-a3b5-37f0ce6a0260").getAnswers(false);
+		Collection<ConceptAnswer> opdAnswers = Context.getConceptService().getConceptByUuid("03880388-07ce-4961-abe7-0e58f787dd23").getAnswers(false);
+		Collection<ConceptAnswer> specialClinicAnswers = Context.getConceptService().getConceptByUuid("b5e0cfd3-1009-4527-8e36-83b5e902b3ea").getAnswers(false);
+
+		for (ConceptAnswer conceptAnswer1 : triageAnswers) {
+			if(conceptAnswer1.getAnswerConcept().equals(concept)) {
+				foundQuestion = conceptAnswer1.getConcept();
+				break;
+			}
+		}
+
+		for (ConceptAnswer conceptAnswer2 : opdAnswers) {
+			if(conceptAnswer2.getAnswerConcept().equals(concept)) {
+				foundQuestion = conceptAnswer2.getConcept();
+				break;
+			}
+		}
+
+		for (ConceptAnswer conceptAnswer3 : specialClinicAnswers) {
+			if(conceptAnswer3.getAnswerConcept().equals(concept)) {
+				foundQuestion = conceptAnswer3.getConcept();
+				break;
+			}
+		}
+
+		return foundQuestion;
 	}
 }
