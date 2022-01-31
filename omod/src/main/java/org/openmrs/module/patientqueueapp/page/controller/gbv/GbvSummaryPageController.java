a9941044998fde22b2ee6123a693a7960229c6c0
ningosi <nicotwendelee@gmail.com>
2022-01-31 14:47:56 +0300

    Provide 2.x references to the module

--- omod/src/main/java/org/openmrs/module/patientqueueapp/page/controller/gbv/GbvSummaryPageController.java
+++ omod/src/main/java/org/openmrs/module/patientqueueapp/page/controller/gbv/GbvSummaryPageController.java
@@ -2,7 +2,7 @@ package org.openmrs.module.patientqueueapp.page.controller.gbv;
 
 import org.openmrs.module.kenyaui.annotation.AppPage;
 
-@AppPage("patientqueueapp.gbv")
+@AppPage("patientqueueapp.sgbv")
 public class GbvSummaryPageController {
 
     public void controller() {
